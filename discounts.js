1f55aef993ac649dfe6ba4f17d205f4651011dff
Albert <avalls89@gmail.com>
2021-12-09 21:00:24 +0100

    09/12/21

--- discounts.js
+++ discounts.js
@@ -1,19 +1,23 @@
-const originalPrice = 120;
-const discount = 18;
+// const originalPrice = 120;
+// const discount = 18;
 
-function calculePriceWithDiscount(originalPrice, discount) { 
-const ratePriceWithDiscount = 100 - discount;
-const priceWidthDiscount = (originalPrice * ratePriceWithDiscount) / 100;
+function calculePriceWithDiscount(price, discount) {
+    const ratePriceWithDiscount = 100 - discount;
+    const priceWidthDiscount = (price * ratePriceWithDiscount) / 100;
 
-return priceWidthDiscount;
+    return priceWidthDiscount;
 };
- 
-function onClickButtonPriceDiscount(){
+
+function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("InputPrice");
-    const priceValue = inputPrice.value; 
-    
-    const inputPrice = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value; 
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
 
     const priceWidthDiscount = calculePriceWithDiscount(priceValue, discountValue);
+
+    const resultPrice = document.getElementById("ResultP");
+    resultPrice.innerText = "The price with discount is: " + priceWidthDiscount;
+
 };
